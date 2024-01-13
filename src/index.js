654166a80b3d3139f57aa8b3a2ad81444afaeefe
Vishwajeet Tripathi <tripathi.vishwajeet12345@gmail.com>
2024-01-13 23:34:23 +0530

    utils codes added

--- src/index.js
+++ src/index.js
@@ -8,7 +8,16 @@ dotenv.config({
 })
 
 
-connectDB();
+connectDB()
+.then(() => {
+    application.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port ${process.env.PORT}`);
+    })
+})
+.catch((err) =>
+{
+    console.log(`MONGODB connection failed ! : ${err}`);
+});
 
 
 // import express from 'express';
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+const asyncHandler = (requestHandler) => {
+
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+        .catch((err) => next)
+    }
+}
+
+
+export {asyncHandler}
+
+
+
+
+// const asyncHandler = (fn) => async (req, res, next)
+// {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
