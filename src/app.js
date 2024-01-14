21b872ef36b9db92eafc515224818141be88d669
Vishwajeet Tripathi <tripathi.vishwajeet12345@gmail.com>
2024-01-14 21:57:48 +0530

    user route and controllers discussed

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
 }))
 
 app.use(express.json({limit:"16kb"}))
@@ -14,7 +15,14 @@ app.use(express.static("public"))
 app.use(cookieParser)
 
 
+// importing routes
 
+import userRouter from './routes/user.routes.js'
+
+
+// routes declaration
+
+app.use("/api/v1/users", userRouter)
 
 
 
